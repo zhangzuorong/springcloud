726389f688805d8edb322f4284a13ca95607d4f7
zhangzuorong <994255496@qq.com>
2018-08-21 18:16:03 +0800

    no message

---
+++ serice-feign/src/main/java/springcloud/sericefeign/cofig/SchedualServiceHi.java
@@ -0,0 +1,21 @@
+package springcloud.sericefeign.cofig;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 开发公司：青岛上朝信息科技有限公司
+ * 版权：青岛上朝信息科技有限公司
+ * <p>
+ * 类功能描述
+ *
+ * @author zhangzuorong
+ * @created 2018/8/21.
+ */
+@FeignClient(value = "service-hi")
+public interface SchedualServiceHi {
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
---
+++ serice-feign/src/main/java/springcloud/sericefeign/controller/MyController.java
@@ -0,0 +1,27 @@
+package springcloud.sericefeign.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import springcloud.sericefeign.cofig.SchedualServiceHi;
+
+/**
+ * 开发公司：青岛上朝信息科技有限公司
+ * 版权：青岛上朝信息科技有限公司
+ * <p>
+ * 类功能描述
+ *
+ * @author zhangzuorong
+ * @created 2018/8/21.
+ */
+@RestController
+public class MyController {
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return schedualServiceHi.sayHiFromClientOne( name );
+    }
+}
