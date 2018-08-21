f04484a6b411ca49644c4e52e83485879d5a52e5
zhangzuorong <994255496@qq.com>
2018-08-21 14:27:35 +0800

    no message

---
+++ service-hi/src/main/java/springcloud/servicehi/controller/MyController.java
@@ -0,0 +1,22 @@
+package springcloud.servicehi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 开发公司：青岛上朝信息科技有限公司
+ * 版权：青岛上朝信息科技有限公司
+ * <p>
+ * 类功能描述
+ *
+ * @author zhangzuorong
+ * @created 2018/8/21.
+ */
+@RestController
+public class MyController {
+    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    public String hi(String name){
+        return "hi:"+name;
+    }
+}
