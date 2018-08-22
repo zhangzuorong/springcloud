e62776a9648a49227f4229392356555fc5530d7e
zhangzuorong <994255496@qq.com>
2018-08-22 13:34:51 +0800

    断路器

--- serice-feign/src/main/java/springcloud/sericefeign/cofig/SchedualServiceHi.java
+++ serice-feign/src/main/java/springcloud/sericefeign/cofig/SchedualServiceHi.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author zhangzuorong
  * @created 2018/8/21.
  */
-@FeignClient(value = "service-hi")
+@FeignClient(value = "service-hi",fallback = SchedualServiceHiHystric.class)
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
     String sayHiFromClientOne(@RequestParam(value = "name") String name);
---
+++ serice-feign/src/main/java/springcloud/sericefeign/cofig/SchedualServiceHiHystric.java
@@ -0,0 +1,20 @@
+package springcloud.sericefeign.cofig;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * 开发公司：青岛上朝信息科技有限公司
+ * 版权：青岛上朝信息科技有限公司
+ * <p>
+ * 类功能描述
+ *
+ * @author zhangzuorong
+ * @created 2018/8/22.
+ */
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "哈哈"+name+" 出错了";
+    }
+}
