0edbc911cbb1fe640a96387673e530da174616cc
zhangzuorong <994255496@qq.com>
2018-08-31 15:43:39 +0800

    我最棒

--- config-client/src/main/java/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/springcloud/configclient/ConfigClientApplication.java
@@ -3,11 +3,17 @@ package springcloud.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
+@EnableDiscoveryClient
+@RefreshScope
 @SpringBootApplication
 @RestController
 public class ConfigClientApplication {
